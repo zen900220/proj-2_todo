d8de83d955b5be8f96f57aea224be3677a7cd17b
zen900220 <59685788+zen900220@users.noreply.github.com>
2023-08-11 02:52:01 +0530

    Added error message for no env found

--- database/utils.js
+++ database/utils.js
@@ -1,8 +1,20 @@
+import { CustomError } from "@/utils/CustomError";
 import mongoose from "mongoose";
 
 export const connectDb = async function () {
+  // check if env vars re working
+  if (!process.env.DB_URL) {
+    throw new CustomError(
+      "Server Error",
+      500,
+      "Can't connect to url: undefined"
+    );
+  }
+
+  // check if a connection is already connected(1) or connecting(2)
   const ready = mongoose.connection.readyState;
   if (ready === 1 || ready === 2) return;
+
   mongoose.set("strictQuery", false);
   const data = await mongoose.connect(process.env.DB_URL);
   console.log(`Connected to mongoose server at: ${data.connection.host}`);
